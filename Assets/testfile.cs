db95a5595319458d9a689f9c79f735a3ad1feda3
sosuyeong <duckyoung.hs@gmail.com>
2025-09-04 20:15:23 +0900

    테스트입니다.
    +testfile

---
+++ Assets/testfile.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class testfile : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //테스트 입니다.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
